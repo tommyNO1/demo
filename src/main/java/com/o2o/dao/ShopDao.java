750acd23d2b34d1c858afe60263da2b70569586c
tommyNO1 <905390519@qq.com>
2020-07-18 16:36:44 +0800

    在Dao层完成了新增店铺的方法并且通过junit进行验证

---
+++ src/main/java/com/o2o/dao/ShopDao.java
@@ -0,0 +1,11 @@
+package com.o2o.dao;
+
+import com.o2o.entity.Shop;
+
+public interface ShopDao {
+    /*
+    * 新增店铺
+    * @param shop
+    * */
+    int insertShop(Shop shop);
+}
---
+++ src/test/com/o2o/dao/ShopDaoTest.java
@@ -0,0 +1,45 @@
+package com.o2o.dao;
+
+import com.o2o.BaseTest;
+import com.o2o.entity.Area;
+import com.o2o.entity.PersonInfo;
+import com.o2o.entity.Shop;
+import com.o2o.entity.ShopCategory;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShopDaoTest extends BaseTest {
+    @Autowired
+    private ShopDao shopDao;
+
+    @Test
+    public void testInsertShop() {
+        Shop shop = new Shop();
+        PersonInfo owner = new PersonInfo();
+        Area area = new Area();
+        ShopCategory shopCategory = new ShopCategory();
+        owner.setUserId(1L);
+        area.setAreaId(2);
+        shopCategory.setShopCategoryId(1L);
+        shop.setOwner(owner);
+        shop.setArea(area);
+        shop.setShopCategory(shopCategory);
+        shop.setShopName("测试店铺");
+        shop.setShopDesc("test");
+        shop.setShopDesc("test");
+        shop.setPhone("test");
+        shop.setShopImg("test");
+        shop.setPriority(1);
+        shop.setAdvice("审核中");
+        shop.setEnableStatus(1);
+        shop.setCreateTime(new Date());
+        int effectedNum = shopDao.insertShop(shop);
+        assertEquals(1,effectedNum);
+
+
+    }
+}
