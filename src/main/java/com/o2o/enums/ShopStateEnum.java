f81f4c86a5592e87863fb22bf6822a9ab3459a27
sheldon <kid87927605@gmail.com>
2020-07-30 12:00:06 +0800

    修改位置

--- src/main/java/com/o2o/enums/ShopStateEnum.java
+++ src/main/java/com/o2o/enums/ShopStateEnum.java
@@ -1,8 +1,11 @@
 package com.o2o.enums;
 
 public enum ShopStateEnum {
-    CHECK(0, "审核中"), OFFLINE(-1, "非法店铺"), SUCCESS(1, "操作成功"),
-    PASS(2, "通过认证"), INNER_ERROR(-1001, "内部系统错误"), NULL_SHOPID(-1002, "ShopID为空"),
+    CHECK(0, "审核中"),
+    OFFLINE(-1, "非法店铺"),
+    SUCCESS(1, "操作成功"),
+    PASS(2, "通过认证"),
+    INNER_ERROR(-1001, "内部系统错误"), NULL_SHOPID(-1002, "ShopID为空"),
     NULL_SHOP(-1003,"shop为空");
 
     private int state;
