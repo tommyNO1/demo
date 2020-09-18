396d8bbea06889621533fc7abaa964e50c0ea51b
tommyNO1 <905390519@qq.com>
2020-09-18 23:05:34 +0800

    增加微信登陆用户信息的获取

--- src/main/java/com/o2o/web/wechat/WechatController.java
+++ src/main/java/com/o2o/web/wechat/WechatController.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @Controller
-@RequestMapping("wechar")
+@RequestMapping("wechat")
 public class WechatController {
     private static Logger log = LoggerFactory.getLogger(WechatController.class);
 
